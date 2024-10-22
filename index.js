a2ea4d6da4e78705e7bdc2d0c1a81308bf7845a8
MGCESAR <151561332+MGCESAR@users.noreply.github.com>
2024-10-21 18:39:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening FLRP Utilities", "Playing FLRP Utilities"];
+const statusMessages = ["🎧 Listening Felon RP", "Playing Felon RP"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
